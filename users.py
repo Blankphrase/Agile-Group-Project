0172e73f0c59610a3c5abbe599ee80fa9ed8429c
Allan Imire <imireallan@gmail.com>
2018-07-30 13:22:34 +0300

    [Feature #159394394] adding signup

--- users.py
+++ users.py
@@ -3,8 +3,22 @@ import datetime
 class Project(object):
     def __init__(self):
         self.user_details = {}
-        # stores credentials
-        self.credentials = {}
         # stores logged in users
         self.session = {}
-    
+
+     def register(self):
+     	firstname = input("Enter your firstname: ")
+     	lastname = input("Enter your lastname: ")
+     	username = input("Enter your username: ")
+     	email = input("Enter your email: ")
+     	password = input("Enter your password: ")
+     	self.user_details.update({
+     		user: {
+     		"firstname":firstname,
+     		"lastname":lastname,
+     		"username":username,
+     		"email":email,
+     		"password":password
+     		}
+     		})
+        print("User successfully created")
